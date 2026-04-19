409ad6f298b99af3c3efcd18387ab81c2f82a283
Raul Vitor - ADS <rvt.gc13620@gmail.com>
2026-04-19 13:23:47 -0300

    Delete logica-basica/tabuada.py

--- logica-basica/tabuada.py
+++
@@ -1,8 +0,0 @@
-def gerar_tabuada():
-    try:
-        num = int(input('escolha um numero da tabuada: '))
-        print(f'\ntabuada do {num}')
-        for i in range(1, 11):
-            print(f'{num} * {i} = {num * i}')
-    except ValueError:
-        print('por favor digite apenas um numero')
