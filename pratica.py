3ec2813bc0b2e9c3ab7f071e1cf4b5580071aa5e
RaulVitor008 <rvt.gc13620@gmail.com>
2026-04-22 10:54:23 -0300

    Adiciona exercicios de logica: pedágio, IMC, tabuada, caixa eletronico

---
+++ pratica.py
@@ -0,0 +1,10 @@
+saldo=float(input('qual o saldo em conta? '))
+saque=float(input('qual o valor do saque? '))
+if saque <= 0:
+    print('sem dinheiro')
+    exit()
+sobrou = saldo - saque
+if saque <= saldo:
+    print(f'Aguarde... contagem... saldo agora é de:{sobrou}')
+else:
+    print('saldo insufiente')
